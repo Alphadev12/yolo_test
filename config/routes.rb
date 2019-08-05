033db5ac3100f21e9fd5ea0025a8e55163c8f8b6
Alpha <ravalasonn@gmail.com>
2019-08-05 12:44:08 +0300

    Création route contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/static_pages/contact', to: 'static_pages#contact'
 end
